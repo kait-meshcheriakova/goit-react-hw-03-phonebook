d4ef233703c249d07813aa5d29a869b8e846416a
kait-meshcheriakova <129512123+kait-meshcheriakova@users.noreply.github.com>
2023-10-12 16:58:08 +0300

    ready for checed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -47,7 +47,17 @@ export class App extends Component {
       contact.name.toLowerCase().includes(normalizedFilter)
     );
   };
-
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts !== null) {
+      this.setState({ contacts: JSON.parse(savedContacts) });
+    }
+  }
+  componentDidUpdate(prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
   removeContact = contactId => {
     this.setState(prevState => {
       return {
